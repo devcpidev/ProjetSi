eed6c7fe40d955d0502889b716b858285134341b
jamillaye <jamillaye@gmail.com>
2020-02-02 01:32:23 +0000

    some update about proprietaire

--- public/js/proprietaire.js
+++ public/js/proprietaire.js
@@ -43,10 +43,26 @@ $(document).ready(function() {
                 $('#adressesen').append("<i class='fa fa-address' aria-hidden='true'>Adresse du propriétaire :</i></span>"+data.adressesen+"")
                 $('#tel').append("<i class='fa fa-user' aria-hidden='true'> Numéro du propriétaire:</i></span>"+data.tel+"")
                 $('#email').append("<i class='fa fa-envelope' aria-hidden='true'> email du propriétaire :</i></span>"+data.email+"")
-                $('#nomrep').append("<i class='fa fa-user' aria-hidden='true'> Nom du représentant:</i></span>"+data.nomrep+"")
-                $('#telrep').append("<i class='fa fa-phone' aria-hidden='true'> N° Tel du représentant :</i></span>"+data.telrep+"")
-                $('#adresserep').append("<i class='fa fa-address' aria-hidden='true'> Adresse représentant :</i></span>"+data.adresserep+"")
-                $('#emailrep').append("<i class='fa fa-envelope' aria-hidden='true'> email Représentant :</i></span>"+data.emailrep+"")
+                if(data.nomrep!=null){
+                    $('#nomrep').append("<i class='fa fa-user' aria-hidden='true'> Nom du représentant:</i></span>"+data.nomrep+"")
+                }else{
+                    $('#nomrep').append("")
+                }
+                if(data.telrep!=null){
+                    $('#telrep').append("<i class='fa fa-phone' aria-hidden='true'> N° Tel du représentant :</i></span>"+data.telrep+"")
+                }else{
+                    $('#telrep').append("")
+                }
+                if(data.adresserep!=null){
+                    $('#adresserep').append("<i class='fa fa-address' aria-hidden='true'> Adresse représentant :</i></span>"+data.adresserep+"")
+                }else{
+                    $('#adresserep').append("")
+                }
+                if(data.emailrep!=null){
+                    $('#emailrep').append("<i class='fa fa-envelope' aria-hidden='true'> email Représentant :</i></span>"+data.emailrep+"")
+                }else{
+                    $('#emailrep').append("")
+                }
             }
        })
         
